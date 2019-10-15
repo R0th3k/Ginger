95622a225f352e59449f12c3bfba05a60f1668f6
Hektor Peña <dev@hektor.mx>
2019-10-15 00:10:35 -0500

    Function purifyCss en gulpfile que sirve para limpiar el css

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
   sass = require('gulp-dart-sass'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
+  purify = require('gulp-purifycss'),
   sourcemaps = require('gulp-sourcemaps'),
   notify = require('gulp-notify'),
   imagemin = require('gulp-imagemin'),
@@ -59,14 +60,20 @@ gulp.task('sass', function () {
     .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
       cascade: true
     }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.css))
+    // .pipe(sourcemaps.write('../../assets/styles/'))
+    .pipe(gulp.dest(paths.sass))
     .pipe(notify({ message: 'Finished minifying Styles' }))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
+gulp.task('purifyCss', function() {
+  return gulp.src(paths.sass+'style.css')
+    .pipe(purify([paths.js+'/*.js', './**/*.php']))
+    // .pipe(sourcemaps.write('../../assets/styles/'))
+    .pipe(gulp.dest(paths.css));
+});
 
 var jsFiles = [
   paths.scripts + "/lib/jquery-3.4.0.js",
@@ -138,6 +145,7 @@ gulp.task('php', browserSync.reload);
  */
 gulp.task('watch', function () {
   gulp.watch(paths.sass + '**/*.scss', ['sass']);
+  gulp.watch(paths.sass + '**/*.scss', ['purifyCss']);
   gulp.watch(paths.scripts + 'app.js', ['minifyJs']);
   gulp.watch("./**/*.php", ['php']); 
 });
